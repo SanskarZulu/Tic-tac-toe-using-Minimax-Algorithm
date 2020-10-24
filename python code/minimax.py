fa7decb2bf01141f1655d8698ac30110a1134af8
Sans <zulu1999vidhu@gmail.com>
2020-10-24 21:19:55 +0530

    second commit

--- python code/minimax.py
+++ python code/minimax.py
@@ -8,10 +8,7 @@ from os import system
 """
 An implementation of Minimax AI Algorithm in Tic Tac Toe,
 using Python.
-This software is available under GPL license.
-Author: Clederson Cruz
-Year: 2017
-License: GNU GENERAL PUBLIC LICENSE (GPL)
+By Sanskar Sharma
 """
 
 HUMAN = -1
